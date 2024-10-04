680e148cd4887c5de73583f142985923afe6043e
Bijay-Gurung <stha4580@gmail.com>
2024-10-04 11:49:27 +0545

    Learning Basics of C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/* Writing my first hello world program */
+Console.WriteLine("Hello, World!");
+
+string firstName = "Karma";
+string lastName = "Gurung";
+
+/* Using String Interpolination */
+Console.WriteLine($"My name is {firstName} {lastName}.");
+
+/* Using String Typecasting */
+Console.WriteLine("My name is " + firstName + " " + lastName + ".");
+
+/* Using Trim method */
+
+string fruit = "     Apple";
+string toy = "Ball     ";
+string pet = "     Cat     ";
+
+Console.WriteLine($"{fruit.TrimStart()}\n{toy.TrimEnd()}\n{pet.Trim()}");
