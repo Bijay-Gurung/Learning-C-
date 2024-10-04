dd35ad52dfa0f77f34fd8c7d03d58ec9532fdc37
Bijay-Gurung <stha4580@gmail.com>
2024-10-04 13:05:19 +0545

    using numbers, integers, maths

--- Program.cs
+++ Program.cs
@@ -1,32 +1,47 @@
-﻿/* Writing my first hello world program */
-Console.WriteLine("Hello, World!");
+﻿// /* Writing my first hello world program */
+// Console.WriteLine("Hello, World!");
 
-string firstName = "Karma";
-string lastName = "Gurung";
+// string firstName = "Karma";
+// string lastName = "Gurung";
 
-/* Using String Interpolination */
-Console.WriteLine($"My name is {firstName} {lastName}.");
+// /* Using String Interpolination */
+// Console.WriteLine($"My name is {firstName} {lastName}.");
 
-/* Using String Typecasting */
-Console.WriteLine("My name is " + firstName + " " + lastName + ".");
+// /* Using String Typecasting */
+// Console.WriteLine("My name is " + firstName + " " + lastName + ".");
 
-/* Using Trim method */
+// /* Using Trim method */
 
-string fruit = "     Apple";
-string toy = "Ball     ";
-string pet = "     Cat     ";
+// string fruit = "     Apple";
+// string toy = "Ball     ";
+// string pet = "     Cat     ";
 
-Console.WriteLine($"{fruit.TrimStart()}\n{toy.TrimEnd()}\n{pet.Trim()}");
+// Console.WriteLine($"{fruit.TrimStart()}\n{toy.TrimEnd()}\n{pet.Trim()}");
 
-/* Searching String */
-firstName = firstName.Replace("Karma", "Bijay"); // using replace method to replace name Karma to Bijay. 
-Console.WriteLine(firstName);
+// /* Searching String */
+// firstName = firstName.Replace("Karma", "Bijay"); // using replace method to replace name Karma to Bijay. 
+// Console.WriteLine(firstName);
 
-// Searching
-string text = "Something is better than nothing.";
-Console.WriteLine(text.Contains("better"));
+// // Searching
+// string text = "Something is better than nothing.";
+// Console.WriteLine(text.Contains("better"));
 
-Console.WriteLine(text.ToUpper());
-Console.WriteLine(text.ToLower());
-Console.WriteLine($"The Length of text is {text.Length}.");
-Console.WriteLine(text.StartsWith("Something"));
+// Console.WriteLine(text.ToUpper());
+// Console.WriteLine(text.ToLower());
+// Console.WriteLine($"The Length of text is {text.Length}.");
+// Console.WriteLine(text.StartsWith("Something"));
+
+// Using Number integers and Maths
+// int a = 5;
+// int b = 6;
+// int c;
+// c = a + b;
+// Console.WriteLine(c);
+
+// int a = 50000000;
+// int b = 60000000;
+// // long c = (long)a + (long)b; // to deal with massive numbers we can use long.
+
+// long c = checked(a+b); // It check the numbers are overflowed or not. signed integer (-).
+// Console.WriteLine(c);
+
